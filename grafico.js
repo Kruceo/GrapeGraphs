b03e6ff277faa16baf6d0fa4c5631d6f374e03af
Kruceo <rafa2015krueger@gmail.com>
2022-09-01 13:25:31 -0300

    graph with non negative values dont starts from middle of canvas

--- grafico.js
+++ grafico.js
@@ -1,5 +1,3 @@
-
-
 var info = [1, 2, 3, 5, 8, 13]
 var quadro = document.getElementById('grafico');
 var ctx = quadro.getContext("2d")
@@ -7,11 +5,6 @@ var graphColor = 'rgb(125,0,255)'
 var fillColor = 'rgb(125,0,255,0.5)'
 var markColor = 'rgb(0,0,0,0.1)'
 var zeroColor = 'rgb(50,255,50)'
-window.onload = function () {
-    
-    
-}
-
 function makeGraph(canvasId,dataArray) {
     quadro = document.getElementById(canvasId);
     ctx = quadro.getContext("2d")
@@ -19,6 +12,7 @@ function makeGraph(canvasId,dataArray) {
     var width = quadro.offsetWidth
     const bigger = getBigger(dataArray) * 2;
     const minor = getMinor(dataArray) * 2
+    var division = 1;
     var coef = bigger;
     if (bigger < Math.abs(minor)) {
         coef = Math.abs(minor)
@@ -26,12 +20,14 @@ function makeGraph(canvasId,dataArray) {
     quadro.setAttribute('width', width)
     quadro.setAttribute('height', height)
     ctx.clearRect(0, 0, width, height)
+    
     if (minor < 0) {
         ctx.beginPath();
         ctx.strokeStyle = zeroColor;
         ctx.moveTo(0, height / 2);
         ctx.lineTo(width, height / 2);
         ctx.stroke();
+        division = 2;
     }
     ctx.strokeStyle = markColor;
     ctx.lineWidth = 2;
@@ -39,33 +35,25 @@ function makeGraph(canvasId,dataArray) {
     //ctx.moveTo(0, height)
     ctx.fillStyle = fillColor
     var xIn = (width / (dataArray.length - 1))
-    var yIn = height / (coef *1.25);
+    var yIn = height / (coef *1.25) * (3 - division);
     for (var y = 0; y < dataArray.length; y++) {
         ctx.moveTo(y * xIn, height);
         ctx.lineTo(y * xIn, 0);
     }
     ctx.stroke();
-
-
-
     ctx.beginPath();
-    ctx.strokeStyle = graphColor
-    ctx.moveTo(0, height / 2 - dataArray[0] * yIn)
-
+    ctx.strokeStyle = graphColor;
+    
+    ctx.moveTo(0, height / division - dataArray[0] * yIn)
     for (var i = 0; i < dataArray.length; i++) {
 
-        ctx.lineTo((i + 1) * xIn, height / 2 - dataArray[i + 1] * yIn)
+        ctx.lineTo((i + 1) * xIn, height / division - dataArray[i + 1] * yIn)
     }
-
     ctx.lineTo(width, height)
-
     ctx.lineTo(0, height)
-    ctx.lineTo(0, height / 2 - dataArray[0] * yIn)
+    ctx.lineTo(0, height / division - dataArray[0] * yIn)
     ctx.stroke();
     ctx.fill()
-
-
-
 }
 function getBigger(infoToSort) {
     var max = 0;
